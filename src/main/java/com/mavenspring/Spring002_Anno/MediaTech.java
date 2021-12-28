3729a410fdb1e7caaacb763c0f3cc2262090da8a
sudipto <sudipto.c@heraizen.com>
2021-12-28 17:50:11 +0530

    @Primary to resolve ambiguity

--- src/main/java/com/mavenspring/Spring002_Anno/MediaTech.java
+++ src/main/java/com/mavenspring/Spring002_Anno/MediaTech.java
@@ -1,8 +1,10 @@
 package com.mavenspring.Spring002_Anno;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class MediaTech implements MobileProcessor {
 
 	public void process() {
